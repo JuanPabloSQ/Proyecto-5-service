cd5e9ad3b36dd8e92a8eaf7dbb0313dac1171d8b
JP Sherka <jp.sherka@gmail.com>
2024-05-05 15:49:43 -0400

    fixed plants

--- controller/institutions.js
+++ controller/institutions.js
@@ -1,13 +1,10 @@
-import InstitutionModel from '../model/institutions.js';
-import ClientModel from '../model/clients.js';
-import UserModel from '../model/users.js';
+import PlantModel from '../model/plants.js';
+import multer from 'multer';
 
 const getAll = async (req, res) => {
   try {
-    const userIds = req.user.sub;
-    const user = await UserModel.getById(userIds);
-    const institutions = await InstitutionModel.getByIds(user.institutions);
-    res.status(200).json({ status: 200, data: institutions });
+    const clients = await PlantModel.getAll();
+    res.status(200).json({ status: 200, data: clients });
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: 500, error: 'Internal Server Error' });
@@ -16,36 +13,115 @@ const getAll = async (req, res) => {
 
 const getById = async (req, res) => {
   try {
-    const institutionId = req.params.id;
-    const Institution = await InstitutionModel.getById(institutionId);
-    console.log(Institution);
-    if (!Institution) {
-      return res
-        .status(404)
-        .json({ status: 404, error: 'Institution not found' });
+    const plantId = req.params.id;
+    const Plant = await PlantModel.getById(plantId);
+    console.log(Plant);
+    if (!Plant) {
+      return res.status(404).json({ status: 404, error: 'Plant not found' });
     }
-    res.status(200).json(Institution);
+    res.status(200).json(Plant);
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: 500, error: 'Internal Server Error' });
   }
 };
 
-const getClientsByInstitution = async (req, res) => {
+const create = async (req, res) => {
   try {
-    const institutionId = req.params.id;
-    const clients = await ClientModel.getClientsByInstitutionId(institutionId);
-    console.log(clients, institutionId);
+    const plantData = req.body.data;
 
-    res.status(200).json({ status: 200, data: clients });
+    const createdPlant = await PlantModel.create(plantData);
+
+    res.status(200).json({
+      status: 200,
+      data: createdPlant,
+    });
+  } catch (error) {
+    console.error('Error creating plant:', error);
+    res.status(500).json({
+      status: 500,
+      error: 'Internal Server Error',
+    });
+  }
+};
+
+const deletePlant = async (req, res) => {
+  try {
+    const plantId = req.params.id;
+    const deletedPlant = await PlantModel.deleteById(plantId);
+
+    if (!deletedPlant) {
+      return res.status(404).json({ status: 404, data: 'Plant not found' });
+    }
+
+    res.status(200).json({ status: 200, data: 'Plant deleted' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: 500, error: 'Internal Server Error' });
   }
+};
+
+const updatePlant = async (req, res) => {
+  try {
+    const plantId = req.params.id;
+    const plantData = req.body.data;
+
+    const existingPlant = await PlantModel.getById(plantId);
+
+    if (!existingPlant) {
+      return res.status(404).json({ status: 404, message: 'Client not found' });
+    }
+
+    const updatedPlant = await PlantModel.updateById(plantId, plantData);
+
+    res.status(200).json({ status: 200, data: updatedPlant });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: 500, error: 'Internal Server Error' });
+  }
+};
+
+const updatePlantImage = (req, res, next) => {
+  const imageDir = 'uploads/plant/images';
+
+  const upload = multer({
+    storage: multer.diskStorage({
+      destination: imageDir,
+      filename: (req, file, cb) => cb(null, `${req.params.id}.png`),
+    }),
+    limits: { fileSize: 1000 * 1000 * 10 },
+    fileFilter: (req, file, cb) => {
+      if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        return res
+          .status(500)
+          .json({ status: 500, error: 'File Format not allowed' });
+      }
+      cb(undefined, true);
+    },
+  }).single('file');
+
+  upload(req, res, async (err) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ status: 500, error: 'Internal Server Error' });
+
+    updatePlant(
+      {
+        ...req,
+        body: { data: { image: `/${imageDir}/${req.params.id}.png` } },
+      },
+      res,
+      next,
+    );
+  });
 };
 
 export default {
   getAll,
   getById,
-  getClientsByInstitution,
+  create,
+  deletePlant,
+  updatePlant,
+  updatePlantImage,
 };
---
+++ model/plants.js
@@ -0,0 +1,51 @@
+import { Schema, model } from 'mongoose';
+
+const plantSchema = new Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    image: {
+      required: true,
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const PlantModel = model('plants', plantSchema);
+
+const getAll = async () => {
+  return PlantModel.find({});
+};
+
+const getById = async (id) => {
+  return PlantModel.findById(id);
+};
+
+const getByIds = async (ids) => {
+  return PlantModel.find({ _id: { $in: ids } });
+};
+
+const create = async (plantData) => {
+  const newPlant = new PlantModel(plantData);
+  return newPlant.save();
+};
+
+const updateById = async (id, plantData) => {
+  return PlantModel.findByIdAndUpdate(id, plantData, { new: true });
+};
+
+const deleteById = async (id) => {
+  return PlantModel.findByIdAndDelete(id);
+};
+
+export default {
+  getAll,
+  getById,
+  getByIds,
+  create,
+  updateById,
+  deleteById,
+};
