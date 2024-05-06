5eb185a8bcecfa7dfcd347553f271972a540272e
JP Sherka <jp.sherka@gmail.com>
2024-05-06 14:51:25 -0400

    fixed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('[{ name: "institucion2", image: "http://image.com" }]');
+  res.send('[{ name: "plants", image: "http://image.com" }]');
 });
 
 export default router;
---
+++ routes/plants.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import PlantsController from '../controller/plants.js';
+import { verifyToken } from '../middleware/auth.js';
+const router = Router();
+
+router.get('/', verifyToken, PlantsController.getAll);
+router.post('/', verifyToken, PlantsController.create);
+router.delete('/:id', verifyToken, PlantsController.deletePlant);
+router.patch('/:id', verifyToken, PlantsController.updatePlant);
+router.post('/:id/image', verifyToken, PlantsController.updatePlantImage);
+
+export default router;
