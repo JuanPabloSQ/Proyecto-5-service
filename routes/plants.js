62dbc63038a6e4cc310d1e813705f683ae8348e9
JP Sherka <jp.sherka@gmail.com>
2024-05-09 00:41:47 -0400

    fixed id plants

--- routes/plants.js
+++ routes/plants.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get('/', verifyToken, PlantsController.getAll);
 router.post('/', verifyToken, PlantsController.create);
+router.get('/:id', verifyToken, PlantsController.getById);
 router.delete('/:id', verifyToken, PlantsController.deletePlant);
 router.patch('/:id', verifyToken, PlantsController.updatePlant);
 router.post('/:id/image', verifyToken, PlantsController.updatePlantImage);
